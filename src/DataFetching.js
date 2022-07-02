d66e6ede1d222b6f0784a3a321a69af0d6606e88
Szymon <s21355@pjwstk.edu.pl>
2022-07-02 16:00:34 +0200

    ew

--- src/DataFetching.js
+++ src/DataFetching.js
@@ -3,28 +3,52 @@ import "./style.css";
 import Post from './records.json';
 
 export default function DataFetching() {
+  const [posts, setPosts] = useState([]);
 
+  const url = "https://jsonplaceholder.typicode.com/posts";
+
+  useEffect(() => {
+    fetch(url)
+      .then(res => {
+        if (!res.ok) {
+          return Error("Oh no");
+        }
+        return res.json();
+      })
+      .then(data => setPosts(data));
+  });
+
+  return (
+    <div className="post">
+      <h1>Posts</h1>
+      <h2>...are here</h2>
+
+      {posts.map(post => (
+        <div key={post.id}>{post.title}</div>
+      ))}
+    </div>
+  );
+}
+/*
     const [posts, setPosts] = useState([]);
     const [randomPost, setRandomPost] = useState(null)
 
     useEffect(() => {    
-        fetch('records.json')
+        fetch('http://jsonplaceholder.typicode.com/users/1/posts')
             .then(res => res.json())
             .then(data => setPosts(data.posts));
     }, []);
 
     return (
     <div className="post">
-        
+        <>
         <button
           onClick={() => {
-            
             const randomId = Math.round(Math.random())
             setRandomPost(posts[randomId])
-            
+            console.log(randomId)
           }}
         >
-          
           Get random post
         </button>
         {randomPost && (
@@ -40,8 +64,8 @@ export default function DataFetching() {
                 <p>{post.body}</p>
             </li>
         </ul>
-        ))} */}
-        
+        ))} }
+        </>
     </div>
     )
-}
+}*/
